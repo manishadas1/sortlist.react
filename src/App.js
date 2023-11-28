0339bd393ddd5fa1ab8e30381cdeed9eece620ee
manishadas1 <manishadas.akrabhi@gmail.com>
2023-11-28 11:02:21 +0530

    sortlist app completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import './App.css'; 
 
-function App() {
+const Types = {
+  ITEM: 'item',
+};
+
+const ListItem = ({ item, index, moveItem }) => {
+  const [, ref] = useDrag({
+    type: Types.ITEM,
+    item: { index },
+  });
+
+  const [, drop] = useDrop({
+    accept: Types.ITEM,
+    hover: (draggedItem) => {
+      if (draggedItem.index !== index) {
+        moveItem(draggedItem.index, index);
+        draggedItem.index = index;
+      }
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={(node) => ref(drop(node))} className="list-item">
+      {item}
     </div>
   );
-}
+};
+
+
+const SortableList = () => {
+  const initialItems = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  const [items, setItems] = useState(initialItems);
+
+  const moveItem = (fromIndex, toIndex) => {
+    const updatedItems = [...items];
+    const [movedItem] = updatedItems.splice(fromIndex, 1);
+    updatedItems.splice(toIndex, 0, movedItem);
+    setItems(updatedItems);
+  };
+
+  return (
+    <div className="sortable-list">
+      {items.map((item, index) => (
+        <ListItem key={index} item={item} index={index} moveItem={moveItem} />
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div>
+        <h1>Sortable List</h1>
+        <SortableList />
+      </div>
+    </DndProvider>
+  );
+};
 
 export default App;
